06f9d6d553da478fee9927fa2ab67b33a1d3c8ce
YoungKwang-Kim <kyk01910691@gmail.com>
2023-11-29 14:24:37 +0900

    중간점검
    날라가기 방지

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,86 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // 계산기 디스플레이
+    public TMP_Text displayText;
 
-    // Update is called once per frame
-    void Update()
+    // 연산자를 담을 변수
+    private List<string> operators = new List<string>();
+
+    // 피연산자를 담을 변수
+    private List<string> operands = new List<string>();
+
+    // 연산자를 입력했을 때 해당 연산자와 이전 피연산자 저장을 위한 변수
+    private string lastOperand;
+    private string lastOperator;
+
+    // 버튼 클릭 이벤트
+    public void OnClickButton(string buttonStr)
     {
-        
+        switch(buttonStr)
+        {
+            case "0":
+            case "1":
+            case "2":
+            case "3":
+            case "4":
+            case "5":
+            case "6":
+            case "7":
+            case "8":
+            case "9":
+                if (lastOperator != null && lastOperand != null)
+                {
+                    operands.Add(lastOperand);
+                    operators.Add(lastOperator);
+
+                    lastOperator = null;
+                    lastOperand = null;
+
+                    displayText.text = "0";
+                }
+
+                if (displayText.text == "0")
+                {
+                    displayText.text = buttonStr;
+                }
+                else
+                {
+                    displayText.text += buttonStr;
+                }
+
+                break;
+
+            case ".":
+                displayText.text += buttonStr;
+                break;
+            case "c":
+                displayText.text = "0";
+                break;
+
+            case "+":
+            case "-":
+            case "*":
+            case "/":
+                lastOperand = displayText.text;
+                lastOperator = buttonStr;
+                break;
+            case "=":
+                operands.Add(displayText.text);
+
+                while (operators.Count > 0)
+                {
+                    int multiplyIndex = operators.IndexOf("*");
+                    int divideIndex = operands.IndexOf("/");
+                    Debug.Log(multiplyIndex);
+                    Debug.Log(divideIndex);
+                }
+
+                break;
+        }
     }
 }
